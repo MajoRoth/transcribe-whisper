8c9d985266834ca7b8e24356dbbd0350c4a52c19
amitroth <amitroth@gmail.com>
2023-06-19 12:32:46 +0300

    lang

--- orig.py
+++ orig.py
@@ -3,8 +3,25 @@ import whisper
 model = whisper.load_model("large-v2")
 
 # load audio and pad/trim it to fit 30 seconds
-result = model.transcribe("/cs/labs/adiyoss/amitroth/vall-e/data/reference/saspeech/amit.wav")
-print(result)
+try:
+    print("Hebrew")
+    result = model.transcribe("/cs/labs/adiyoss/amitroth/vall-e/data/reference/saspeech/amit.wav", language='Hebrew')
+    print(result)
 
-# print the recognized text
-print(result.text)
+    # print the recognized text
+    print(result.text)
+
+except Exception as e:
+    print(e)
+
+# load audio and pad/trim it to fit 30 seconds
+try:
+    print("hebrew")
+    result = model.transcribe("/cs/labs/adiyoss/amitroth/vall-e/data/reference/saspeech/amit.wav", language='hebrew')
+    print(result)
+
+    # print the recognized text
+    print(result.text)
+
+except Exception as e:
+    print(e)
