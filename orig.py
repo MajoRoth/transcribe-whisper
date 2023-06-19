484aca6e13ba158b887c455a176a1002149c1a1f
amitroth <amitroth@gmail.com>
2023-06-19 11:24:24 +0300

    whisper

---
+++ orig.py
@@ -0,0 +1,27 @@
+import whisper
+
+model = whisper.load_model("base")
+
+# load audio and pad/trim it to fit 30 seconds
+audio = whisper.load_audio("/cs/labs/adiyoss/amitroth/vall-e/data/reference/saspeech/amit.wav")
+# audio = whisper.pad_or_trim(audio)
+
+print("Loaded Audio")
+print(audio)
+
+# make log-Mel spectrogram and move to the same device as the model
+mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+print("Loaded Mel")
+print(mel)
+
+# detect the spoken language
+_, probs = model.detect_language(mel)
+print(f"Detected language: {max(probs, key=probs.get)}")
+
+# decode the audio
+options = whisper.DecodingOptions()
+result = whisper.decode(model, mel, options)
+
+# print the recognized text
+print(result.text)
